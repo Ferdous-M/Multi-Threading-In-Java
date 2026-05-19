f973ab20ed199d34ad2ab9c20eb94b28231f4f86
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-19 17:21:15 +0600

    multiple task

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        MyThread t1 = new MyThread();
+        Thread t1 = new Thread(new MyTask());
 
-        //t1.start();
+        t1.start();
 
-        Thread t2 = new Thread(new MyTask());
+        Thread t2 = new Thread(new MyTask2());
 
         t2.start();
     }
