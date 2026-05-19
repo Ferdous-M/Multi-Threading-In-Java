ef29f48739346cb16cbd72180a66788d700645a3
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-19 17:16:35 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    MyThread t1 = new MyThread();
+
+    t1.start();
+    }
+}
