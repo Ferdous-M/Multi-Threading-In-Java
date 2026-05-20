7671633cc9c4024ef73d16fef3b1dd0f66f76e7f
Ferdous-M <mahzabinferdous000@gmail.com>
2026-05-20 12:12:36 +0600

    update thread

--- src/MyThread.java
+++ src/MyThread.java
@@ -3,16 +3,18 @@ public class MyThread extends Thread {
     @Override
     public void run() {
         Thread.yield();
-        for (int i = 0; i < 5; i++) {
-            System.out.println("child Thread is running: " + i + "  "+Thread.currentThread().getName());
-        }
+
         try
         {
-            Thread.sleep(1000);
+            Thread.sleep(3000);
         }
         catch (InterruptedException e)
         {
             e.printStackTrace();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("child Thread is running: " + i + "  "+Thread.currentThread().getName());
         }
     }
 }
